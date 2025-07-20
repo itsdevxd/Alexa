d1fb72cce05b1d2675d58ccdd5070c0c949fc73b
• 𝑇𝑜𝑥𝑖𝑛 \ 🇮🇳 <btwdevxd@gmail.com>
2025-07-20 21:06:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/midoriupdates")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+IUB5iIuLoOA3YTAx")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/do_pal0")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/xscnox")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -60,6 +60,11 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
+# API URL - here
+API_URL = getenv("API_URL", "https://tgmusic.fallenapi.fun")
+
+# API KEY - here
+API_KEY = getenv("API_KEY", "")
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
