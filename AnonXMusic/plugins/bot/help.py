2673ad55b6e86e26a4ec2296fae4e665c4628f29
• 𝑇𝑜𝑥𝑖𝑛 \ 🇮🇳 <btwdevxd@gmail.com>
2025-07-20 21:19:16 +0530

    Update help.py

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -88,3 +88,7 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
     elif cb == "hb15":
         await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+    elif cb == "ud15":
+        await CallbackQuery.reply_photo(
+            photo=START_IMG_URL,reply_markup=keyboard
+        )
