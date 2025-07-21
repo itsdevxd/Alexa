c644cfb5a9140d8ca19c402b0db6c4d2b55b90f3
• 𝑇𝑜𝑥𝑖𝑛 \ 🇮🇳 <btwdevxd@gmail.com>
2025-07-21 12:51:52 +0530

    Update settings.py

--- AnonXMusic/plugins/bot/settings.py
+++ AnonXMusic/plugins/bot/settings.py
@@ -794,6 +794,6 @@ async def gib_repo_callback(_, callback_query):
         media=InputMediaVideo(
             "https://files.catbox.moe/tt3km7.mp4", 
             caption="𝐻𝑎𝑟 𝑘𝑖𝑠𝑖𝑘𝑜 𝑛𝑎ℎ𝑖 𝑚𝑖𝑙𝑡𝑎 𝑦𝑎ℎ𝑎 𝑝𝑦𝑎𝑟 𝑧𝑖𝑛𝑑𝑔𝑖 𝑚𝑒ℎ 💗🫀🌾"),
-            InlineKeyboardButton(text="• ʙᴀᴄᴋ •", callback_data="settingsback_helper"),
-    )
+            InlineKeyboardButton(text="• ʙᴀᴄᴋ •", callback_data="settingsback_helper")),
+    
       return
