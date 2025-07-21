98d97067c31efb17e4304bf26aba09e0c3519798
• 𝑇𝑜𝑥𝑖𝑛 \ 🇮🇳 <btwdevxd@gmail.com>
2025-07-21 15:41:01 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -12,7 +12,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="dil_spy"),
         ],
     ]
     return buttons
@@ -28,12 +28,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
+            InlineKeyboardButton(text=_["S_B_9"], callback_data="dil_spy"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="gib_source"),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="dil_spy"),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/do_pal0"),
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
         ],
     ]
     return buttons
